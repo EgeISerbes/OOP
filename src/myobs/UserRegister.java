1472419b2a3e9787ec40a5b5fa32849a9ff2f9fe
EgeISerbes <42302757+EgeISerbes@users.noreply.github.com>
2019-05-09 13:05:19 +0300

    Update UserRegister.java

--- src/myobs/UserRegister.java
+++ src/myobs/UserRegister.java
@@ -17,6 +17,7 @@ public class UserRegister { // Program ilk çalıştırıldığında kayıt bilg
    protected  String[][] SchoolID = new String[5][2];
    protected  String[][] Username = new String[5][2]; 
    protected  String[][] Password = new String[5][2]; 
+   protected  String[][] Department = new String[5][2];  
    
    
    public void UserRegister() //Constructor
@@ -35,12 +36,16 @@ public class UserRegister { // Program ilk çalıştırıldığında kayıt bilg
        System.out.println("Enter your School ID :");
        SchoolID[ID-1][0] = input.nextLine();
        SchoolID[ID-1][1] = Integer.toString(ID);
+       System.out.print("Enter your Department : ");
+       Department[ID-1][0]= input.nextLine();
+       Department[ID-1][1]=Integer.toString(ID);
        System.out.print("Enter a username : ");
        Username[ID-1][0] = input.nextLine();
        Username[ID-1][1]= Integer.toString(ID);
        System.out.print("\nEnter a Password : ");
        Password[ID-1][0]= input.nextLine();
        Password[ID-1][1]= Integer.toString(ID);
+       
        
        ID ++;
       
